f0e2692ca961ec083a1b0d5c72ede0ac16afb482
Heitor Scalabrini <47569622+heitorsam@users.noreply.github.com>
2023-08-15 17:15:53 -0300

    blade

---
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,13 @@
+<h3>Fornecedor</h3>
+
+{{-- Página Fornecedor --}}
+
+@php
+
+    //Trecho php
+
+    echo 'php puro' . '</br></br>';
+
+@endphp
+
+<?= 'Outro tipo de echo' ?>
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -25,12 +25,24 @@ Route::get('/contato', 'ContatoController@contato')->name('site.sobrenos');
 Route::get('/login', function(){ return 'Login'; })->name('site.contato');
 
 Route::prefix('/app')->group(function(){
-
     Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
-    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
-    
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');    
 });
+
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para a página inicial';    
+});
+
+//Route::redirect('/rota2','/rota1');
+
+/*
+Route::get('/rota2', function() {
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+*/
 
 /*
 Route::get(
@@ -41,8 +53,6 @@ Route::get(
     ) {
         echo 'Estamos aqui ' . $nome . ' - ' . $categoria_id;
     }
-
 )->where('categoria_id','[0-9]+')
  ->where('nome', '[A-Za-z]+');
-
  */
