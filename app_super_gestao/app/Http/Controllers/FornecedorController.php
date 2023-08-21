a29fbb0ac12a9f76c3f4069141f1dbaddce60b27
Heitor Scalabrini <47569622+heitorsam@users.noreply.github.com>
2023-08-21 11:52:25 -0300

    21 08 2023

--- app_super_gestao/app/Http/Controllers/FornecedorController.php
+++ app_super_gestao/app/Http/Controllers/FornecedorController.php
@@ -7,6 +7,14 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index(){
-        return view('app.fornecedor.index');        
+        $fornecedores = [
+            0 => ['nome' => 'Forncedor 1', 'status' => 'N', 'cnpj' => '',
+                  'ddd' => '11', 'telefone' => '0000-0000'],
+            1 => ['nome' => 'Forncedor 2', 'status' => 'N', 'cnpj' => '',
+                  'ddd' => '85', 'telefone' => '0000-0000'],
+            2 => ['nome' => 'Forncedor 3', 'status' => 'N', 'cnpj' => '',
+                  'ddd' => '32', 'telefone' => '0000-0000'],
+        ];
+        return view('app.fornecedor.index', compact('fornecedores'));        
     }
 }
--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -3,11 +3,56 @@
 {{-- Página Fornecedor --}}
 
 @php
-
     //Trecho php
-
     echo 'php puro' . '</br></br>';
-
 @endphp
 
 <?= 'Outro tipo de echo' ?>
+
+{{-- @dd($fornecedores) --}}
+
+@if(count($fornecedores)>0&&count($fornecedores)<10)
+    <h3>Existem entre 0 e 10 fornecedores cadastrados</h3>
+@elseif(count($fornecedores)>10)
+    <h3>Existem mais de 10 fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
+
+@isset($fornecedores)
+
+    @foreach($fornecedores as $row => $fornecedor)
+        Total Iterações: {{ $loop->count }} 
+        Iteração Atual: {{ $loop->iteration }}
+        Fornecedor: {{$fornecedor['nome']}}
+        Status: {{$fornecedor['status']}}
+        @unless($fornecedor['status']=='S')
+            Forncedor inativo
+        @endunless
+        CNPJ: {{ $fornecedor['cnpj'] }}
+        @empty($fornecedor['cnpj'])
+            Vazio
+        @endempty
+        Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+        @switch($fornecedor['ddd'])
+            @case ('11') 
+                São Paulo - SP
+                @break
+            @case ('32') 
+                Juiz de Fora - MG 
+                @break
+            @case ('85') 
+                Fortaleza - CE 
+                @break 
+            @default
+                Estado não identificado       
+        @endswitch
+        @if($loop->first)
+            Primeira Iteração
+        @endif
+        @if($loop->last)
+            Úlima Iteração
+        @endif
+        <hr>
+    @endforeach
+@endisset
