397dcbb8b2e8f88bb727c38584cfd3ded989279b
Heitor Scalabrini <47569622+heitorsam@users.noreply.github.com>
2023-08-22 15:43:43 -0300

    component classe

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -20,9 +20,10 @@ Route::get('/', function () {
 */
 
 Route::get('/', 'PrincipalController@principal')->name('site.index');
-Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.index');
-Route::get('/contato', 'ContatoController@contato')->name('site.sobrenos');
-Route::get('/login', function(){ return 'Login'; })->name('site.contato');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function(){ return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function(){
     Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
